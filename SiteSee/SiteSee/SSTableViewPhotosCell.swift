0376e03abd785f71d4de4ad37f93a5c09cac9e8c
Tom Lai <tomtclai@gmail.com>
2016-03-24 17:19:28 -0700

    will come back later

---
+++ SiteSee/SiteSee/SSTableViewPhotosCell.swift
@@ -0,0 +1,13 @@
+//
+//  SSTableViewPhotosCell.swift
+//  SiteSee
+//
+//  Created by Tom Lai on 3/24/16.
+//  Copyright © 2016 Lai. All rights reserved.
+//
+
+import UIKit
+
+class SSTableViewPhotosCell: UITableViewCell {
+
+}
