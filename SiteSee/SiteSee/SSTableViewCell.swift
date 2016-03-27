11ed69ceaa47bb1e877702d005d714e3e099357d
Tom Lai <tomtclai@gmail.com>
2016-03-27 12:23:33 -0700

    refactor: remove unnecessary code in cell

--- SiteSee/SiteSee/SSTableViewCell.swift
+++ SiteSee/SiteSee/SSTableViewCell.swift
@@ -12,16 +12,4 @@ class SSTableViewCell: UITableViewCell {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
