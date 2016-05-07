36e130633aae8ac13d7d4b0c46518e5627bd83ff
Tom Lai <tomtclai@gmail.com>
2016-05-07 15:41:19 -0700

    wip

--- SiteSee/PhotoViewController.swift
+++ SiteSee/PhotoViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 import SafariServices
 class PhotoViewController: UIViewController {
     var image : Image!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var effectView: UIVisualEffectView!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var attributionLabel: UILabel!
     @IBOutlet weak var attribution: UIButton!
@@ -37,17 +43,44 @@ class PhotoViewController: UIViewController {
         }
         
         imageView.image = UIImage(contentsOfFile: Image.imgPath(uuid))
+        
+        scrollView.delegate = self;
+        setupAttributions()
+        loadFullSizeImage()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        updateMinZoomScaleFor(scrollView.bounds.size)
+    }
+    private func updateConstraintsForSize(size: CGSize) {
+        
+        let yOffset = max(0, (size.height - imageView.frame.height) / 2)
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, (size.width - imageView.frame.width) / 2)
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        view.layoutIfNeeded()
+    }
+    func updateMinZoomScaleFor(size: CGSize) -> Void {
+        let minXScale = size.width / imageView.bounds.width
+        let minYScale = size.height / imageView.bounds.height
+        scrollView.minimumZoomScale = min(minXScale,minYScale)
+        scrollView.zoomScale = scrollView.minimumZoomScale
+    }
+    @IBAction func attributionTapped(sender: UIButton) {
+        let sfv = SFSafariViewController(URL: NSURL(string: Flickr.Constants.licenseUrl(image.license!.integerValue)!)!)
+        navigationController?.pushViewController(sfv, animated: true)
+    }
+    func setupAttributions() -> Void {
         attribution.setTitle(attributionStr(image.license!.integerValue, ownerName:image.ownerName!), forState: .Normal)
         attribution.titleLabel?.textAlignment = .Center
         attributionLabel.text = "Copyright © \(image.ownerName!). No changes were made."
         attributionLabel.textColor = UIColor.blackColor().colorWithAlphaComponent(0.7)
         attribution.titleLabel?.textColor = UIColor.blueColor().colorWithAlphaComponent(0.7)
-        loadFullSizeImage()
-    }
-    
-    @IBAction func attributionTapped(sender: UIButton) {
-        let sfv = SFSafariViewController(URL: NSURL(string: Flickr.Constants.licenseUrl(image.license!.integerValue)!)!)
-        navigationController?.pushViewController(sfv, animated: true)
+        scrollView.scrollIndicatorInsets.bottom = effectView.frame.height;
     }
     func loadFullSizeImage() -> Void {
         guard image.origImageUrl != nil else {
@@ -59,7 +92,7 @@ class PhotoViewController: UIViewController {
                 
                 UIView.transitionWithView(self.imageView, duration:0.2, options: .TransitionCrossDissolve, animations: {
                     self.imageView.image = UIImage(data: data)!
-                    self.scrollView.contentSize = self.imageView.frame.size
+                    self.updateMinZoomScaleFor(self.imageView.bounds.size)
                     }, completion: nil)
                 UIApplication.sharedApplication().networkActivityIndicatorVisible = false
             }
