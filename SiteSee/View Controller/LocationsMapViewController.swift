dcc7fb02928184d66bb9642236e7f28d93569733
Tom Lai <tomtclai@gmail.com>
2017-12-06 13:53:59 -0800

    take out the state abbr map

--- SiteSee/View Controller/LocationsMapViewController.swift
+++ SiteSee/View Controller/LocationsMapViewController.swift
@@ -17,63 +17,6 @@ class LocationsMapViewController: UIViewController {
     var locationManager = CLLocationManager()
     var geocoder = CLGeocoder()
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    let dictionaryStateNames = [
-        "AK" : "Alaska",
-        "AL" : "Alabama",
-        "AR" : "Arkansas",
-        "AS" : "American Samoa",
-        "AZ" : "Arizona",
-        "CA" : "California",
-        "CO" : "Colorado",
-        "CT" : "Connecticut",
-        "DC" : "District of Columbia",
-        "DE" : "Delaware",
-        "FL" : "Florida",
-        "GA" : "Georgia",
-        "GU" : "Guam",
-        "HI" : "Hawaii",
-        "IA" : "Iowa",
-        "ID" : "Idaho",
-        "IL" : "Illinois",
-        "IN" : "Indiana",
-        "KS" : "Kansas",
-        "KY" : "Kentucky",
-        "LA" : "Louisiana",
-        "MA" : "Massachusetts",
-        "MD" : "Maryland",
-        "ME" : "Maine",
-        "MI" : "Michigan",
-        "MN" : "Minnesota",
-        "MO" : "Missouri",
-        "MS" : "Mississippi",
-        "MT" : "Montana",
-        "NC" : "North Carolina",
-        "ND" : " North Dakota",
-        "NE" : "Nebraska",
-        "NH" : "New Hampshire",
-        "NJ" : "New Jersey",
-        "NM" : "New Mexico",
-        "NV" : "Nevada",
-        "NY" : "New York",
-        "OH" : "Ohio",
-        "OK" : "Oklahoma",
-        "OR" : "Oregon",
-        "PA" : "Pennsylvania",
-        "PR" : "Puerto Rico",
-        "RI" : "Rhode Island",
-        "SC" : "South Carolina",
-        "SD" : "South Dakota",
-        "TN" : "Tennessee",
-        "TX" : "Texas",
-        "UT" : "Utah",
-        "VA" : "Virginia",
-        "VI" : "Virgin Islands",
-        "VT" : "Vermont",
-        "WA" : "Washington",
-        "WI" : "Wisconsin",
-        "WV" : "West Virginia",
-        "WY" : "Wyoming"
-    ]
 
     @IBOutlet weak var locationButton: UIBarButtonItem!
     
@@ -314,7 +257,7 @@ class LocationsMapViewController: UIViewController {
         }
         
         if altitude < 10000000, let administrativeArea = placemark.administrativeArea {
-            if let name = dictionaryStateNames[administrativeArea] as? String{
+            if let name = Constants.stateAbbreviationMap[administrativeArea] as? String{
                 names.append(name)
             } else {
                 names.append(administrativeArea)
